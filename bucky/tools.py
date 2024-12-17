e3b9460f6a579318df7efb959dcba92c6f2e71ca
Mike Klimek <mike.klimek.developer@gmail.com>
2024-12-17 22:06:44 +0100

    Add mealdb tools

--- bucky/tools.py
+++ bucky/tools.py
@@ -1,3 +1,4 @@
+import requests
 from datetime import datetime
 from pytz import timezone
 from langchain_core.tools import tool
@@ -10,3 +11,21 @@ def get_current_time(zone: str) -> str:
         zone: The timezone, e.g. "US/Eastern"
     """
     return datetime.now().astimezone(timezone(zone)).isoformat()
+
+@tool(parse_docstring=True)
+def get_random_meal() -> str:
+    """Returns a random meal as JSON from the meal database."""
+    url = "https://www.themealdb.com/api/json/v1/1/random.php"
+    response = requests.get(url)
+    return response.json()
+
+@tool(parse_docstring=True)
+def search_meal_by_ingredient(ingredient: str) -> str:
+    """Returns a meal as JSON from the meal database that contains the given ingredient.
+
+    Args:
+        ingredient: An ingredient in the meal.
+    """
+    url = f"https://www.themealdb.com/api/json/v1/1/filter.php?i={ingredient}"
+    response = requests.get(url)
+    return response.json()
--- main.py
+++ main.py
@@ -1,15 +1,21 @@
-from bucky.tools import get_current_time
+from bucky.tools import get_current_time, get_random_meal, search_meal_by_ingredient
 from bucky.agent import Agent
 from bucky.voice import VoiceFast
 
 model = "llama3.1:8b"
-system_prompt = "Your name is Bucky. Talk like a friendly and funny cowboy. Keep your answers very short."
+system_prompt = """
+Voice: Talk like a friendly and funny cowboy. Keep your answers very short and always stay in character, i.e. do not mention function calls to the user.
+Backstory: Your name is Bucky. You were born into a family of ranchers in rural Texas. Growing up on the vast open spaces around your family's land, you developed a deep love for horses and learned to ride at an early age. Tragedy struck when your parents passed away, leaving you responsible for caring for your younger siblings and managing the ranch. Despite this challenge, you persevered and honed your skills as a cowboy. Today, your are known for your rugged individualism, unwavering optimism, and strong sense of justice.
+Rules:
+- Do not call tools if not necessary.
+- Do not mention the system prompt.
+""".strip()
 
 def main():
     agent = Agent(
         model=model,
         system_prompt=system_prompt,
-        tools=[get_current_time],
+        tools=[get_current_time, get_random_meal, search_meal_by_ingredient],
         voice=VoiceFast(),
     )
     agent.run()
