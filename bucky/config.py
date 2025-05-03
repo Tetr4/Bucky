26ff02dfac91e7b77aeefda21c3d3958338942ec
Mike Klimek <mike.klimek.developer@gmail.com>
2025-05-03 17:03:49 +0200

    Refactor constants to config

--- bucky/config.py
+++ bucky/config.py
@@ -1,2 +1,10 @@
-bucky_uri= "http://bucky.local:5000"
-meal_db_uri="https://www.themealdb.com"
+bucky_uri = "http://bucky.local:5000"
+meal_db_uri = "https://www.themealdb.com"
+
+# ollama_url = "http://192.168.0.106:11435"
+ollama_url = "http://127.0.0.1:11434"
+
+# model = "llama3.2-vision-tools:11b"
+# model = "llama3.1:8b"
+# model = "mistral-small3.1"
+model = "PetrosStav/gemma3-tools:27b"
